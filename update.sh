10683df6bfcd598bf01be00d96365708af77bcdc
user <email@address.com>
2022-07-22 13:39:57 +0900

    storage

---
+++ update.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+rm -rf .git
+git init
+git add -A
+git commit -m "storage" --author="user <email@address.com>"
+git remote add origin https://github.com/woojinjo1/storage.git
+git push -u --force origin master
