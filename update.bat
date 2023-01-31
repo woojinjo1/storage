ee5c257c588cfc983ab041bb95a5b192548d540b
user <email@address.com>
2023-01-31 09:55:56 +0900

    storage

---
+++ update.bat
@@ -0,0 +1,7 @@
+RD /S /Q .git
+git init
+git add -A
+git commit -m "storage" --author="user <email@address.com>"
+git remote add origin https://github.com/woojinjo1/storage.git
+git push -u --force origin master
+pause
